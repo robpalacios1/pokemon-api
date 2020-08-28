897e7672059ff4283ca1cdee4ea40879e012dfd1
roberto <robpalacios1@hotmail.com>
2020-08-27 23:42:10 -0500

    add a new component Card.js

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Card({pokemon}) {
+    return (
+        <div className="Card">
+            <div className="Card__img">
+
+            </div>
+            <div className="Card__Name">
+
+            </div>
+            <div className="Card__types"></div>
+            <div className="Card__info"></div>
+        </div>
+    )
+}
+
+export default Card
